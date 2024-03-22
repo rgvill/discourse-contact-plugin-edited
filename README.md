9b3fd7bcfeaae1ec50d341fa59f7d9e0e05cbf7e
Jon Ericson <jon@jlericson.com>
2024-03-22 00:34:35 -0700

    Not exactly working but at least not erroring out.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 **Plugin Summary**
 
 For more information, please see: **url to meta topic**
+
+
+[How to create a Discourse plugin](https://kleinfreund.de/how-to-create-a-discourse-plugin/)
--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,36 @@
+# coding: utf-8
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name:discourse-contact-plugin
+# about: Add a contact form
 # meta_topic_id: TODO
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: Jon Ericson
+# url: https://github.com/jericson/discourse-contact-plugin
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :contact_form_enabled
+
+register_asset 'stylesheets/contact.scss'
 
 module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+  PLUGIN_NAME = "discourse-contact-plugin"
 end
 
 require_relative "lib/my_plugin_module/engine"
 
+load File.expand_path('../app/contact_store.rb', __FILE__)
+
 after_initialize do
-  # Code which should run after Rails has finished booting
+  load File.expand_path('../app/controllers/contact_controller.rb', __FILE__)
+  load File.expand_path('../app/controllers/contacts_controller.rb', __FILE__)
+
+  Discourse::Application.routes.append do
+    # Map the path `/contact` to `ContactController`’s `index` method
+    get '/contact' => 'contact#index'
+
+    get '/contacts' => 'contacts#index'
+    put '/contacts/:contact_id' => 'contacts#update'
+    delete '/contacts/:contact_id' => 'contacts#destroy'
+  end
 end
