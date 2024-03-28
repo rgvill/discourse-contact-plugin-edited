8201209fcd232dd3dbb17cc4b36fb520edfaf1eb
Jon Ericson <jon@jlericson.com>
2024-03-28 13:54:04 -0700

    Fixed using stree write

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,15 +1,13 @@
 # coding: utf-8
 # frozen_string_literal: true
 class ContactController < ApplicationController
-
-#  skip_before_action :check_xhr, raise: false
+  #  skip_before_action :check_xhr, raise: false
   skip_before_action :check_xhr,
                      #:verify_authenticity_token,
                      #:redirect_to_login_if_required,
                      raise: false
 
-
   def index
-    Rails.logger.info '🚂 Called the `ContactController#index` method.'
+    Rails.logger.info "🚂 Called the `ContactController#index` method."
   end
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,63 +1,59 @@
 # frozen_string_literal: true
 class ContactsController < ApplicationController
-
   skip_before_action :check_xhr,
                      :verify_authenticity_token,
                      :redirect_to_login_if_required,
                      raise: false
 
-
   def index
-    Rails.logger.info 'Called ContactsController#index'
+    Rails.logger.info "Called ContactsController#index"
     contacts = ContactStore.get_contacts()
 
     render json: { contacts: contacts.values }
   end
 
   def update
-    Rails.logger.info 'Called ContactsController#update'
+    Rails.logger.info "Called ContactsController#update"
 
     contact_id = params[:contact_id]
     contact = {
-      'id' => contact_id,
-      'name' => params[:contact][:name],
-      'email' => params[:contact][:email],
-      'phone' => params[:contact][:phone],
-      'message' => params[:contact][:message]
-
+      "id" => contact_id,
+      "name" => params[:contact][:name],
+      "email" => params[:contact][:email],
+      "phone" => params[:contact][:phone],
+      "message" => params[:contact][:message],
     }
 
     ContactStore.add_contact(contact_id, contact)
 
-    @date=Time.now.strftime('%a, %-d %b %Y %H:%M:%S')
+    @date = Time.now.strftime("%a, %-d %b %Y %H:%M:%S")
 
-    @mail="
+    @mail =
+      "
 Date: #{@date}
-From: #{contact['email'] || "unknown@example.com"}
+From: #{contact["email"] || "unknown@example.com"}
 To: jons-biography@beta.buildcivitas.com
-Subject: Contact from #{contact['name']} - #{@date}
+Subject: Contact from #{contact["name"]} - #{@date}
 
 
-Name:  #{contact['name']}
-Phone: #{contact['phone']}
-Email: #{contact['email']}
+Name:  #{contact["name"]}
+Phone: #{contact["phone"]}
+Email: #{contact["email"]}
 
 Message:
 
-> #{contact['message']}"
-
+> #{contact["message"]}"
 
     Mail.new(@mail).message_id.presence
 
     receiver = Email::Receiver.new(@mail)
     receiver.process!
 
-
     render json: { contact: contact }
   end
 
   def destroy
-    Rails.logger.info 'Called ContactsController#destroy'
+    Rails.logger.info "Called ContactsController#destroy"
 
     ContactStore.remove_contact(params[:contact_id])
 
